da1ff870c324f1d83a6ea5056f308080530c2e37
iluxa_com <iluxa_com@mail.ru>
2017-03-09 11:43:19 +0300

    Update 000_version.sql

--- updates/000_version.sql
+++ updates/000_version.sql
@@ -1,6 +1,6 @@
-UPDATE `version` SET
-    `db_version` = 'TDB 434.08',
-    `cache_id` = 8
+UPDATE `version___` SET
+    `db_version___` = 'TDB 434.08',
+    `cache_id___` = 8
 WHERE
-    `db_version` = 'TDB 434.07'
+    `db_version__` = 'TDB 434.07'
 LIMIT 1;
